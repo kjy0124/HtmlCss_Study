d819f8670d9e2e1242cc0c0adeb6a505e34da8c5
Kim-jong-yeop <114080381+kjy0124@users.noreply.github.com>
2025-01-13 18:48:35 +0900

    250113(강의)

---
+++ js강의0113.js
@@ -0,0 +1,28 @@
+// 함수 선언 키워드 : let, const
+// 함수 선언 키워드 : funtion
+
+//---------함수 정의하는 다른 방법------------
+//변수 선언 키워드로 함수를 표현 : 함수표현식
+
+// const addFive = functionå(num) {
+//   return num + 5;
+// }
+// addFive(9);
+
+// let result = addFive(9);
+// console.log(result)
+
+//지금은 아무거나 쓰세요.
+// 나중에는 함수 표현식을 더 많이 써야함.
+//호이스팅 때문
+
+//함수 표현식은 화살표 표현식으로 변형 가능
+// const addFive = (num) => {
+//   return num + 5;
+// };
+
+// const addFive = (num) => num + 5;
+
+
+//함수 : 동사를 정의하는 것
+// 컴퓨터한테 명령핟다는 것 : '동사'를 시키는 것 -> 프로그래밍은 함수 만들기
