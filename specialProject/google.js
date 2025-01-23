02637437ac817369f906524d0446d8f3ffa720cf
Kim-jong-yeop <114080381+kjy0124@users.noreply.github.com>
2025-01-23 11:06:09 +0900

    250123(계산기 프로젝트 마크업 완성_1)

---
+++ specialProject/google.js
@@ -0,0 +1,7 @@
+function Add(x, y) {
+  this.x = x;
+  this.y = y;
+  return 10;
+}
+
+console.log(Add(10, 20));
---
+++ teamPJ_calculator/pj_calculator.js
@@ -0,0 +1,3 @@
+
+const input = document.querySelector('.input');
+const result = document.querySelector('.result');
