168f2f603840a90413b1721edb2f316a78b9ab3a
Kim-jong-yeop <114080381+kjy0124@users.noreply.github.com>
2025-01-30 16:22:41 +0900

    250130(삭제버튼 최종본)

--- teamPJ_calculator/pj_calculator.js
+++ teamPJ_calculator/pj_calculator.js
@@ -16,8 +16,12 @@ buttons.forEach((btn) => {
 
     //btn이 가리키는 버튼의 클래스 중 num이 있는지 확인
     if (btn.classList.contains("num")) {
+      //입력된 숫자가 없이 = 버튼이 눌러졌을 때 0=n 이런 창이 result창에 나오지 않도록 전체초기화 함수 호출
+      if (current_operator === "=") {
+        current_operator = null;
+        allClear();
+      }
       //display 숫자가 0인 경우. 숫자버튼을 클릭했을 때 display숫자 버튼에 클릭한 버튼 값 적용
-
       click_text = parseInt(click_text);
       if (display_num === 0) {
         display_num = click_text;
@@ -26,8 +30,10 @@ buttons.forEach((btn) => {
         //입력한 값 뒤에 자라수 추가를 위해 *10
         display_num = display_num * 10 + click_text;
       }
-      acBtn.textContent = display_num === 0 ? "AC" : "⌫";
+      //result 창에 prev_num과 연산자 그리고 새로 입력한 display_num 값이 나오도록
+      result.textContent = prev_num + current_operator + display_num;
       input.textContent = display_num;
+      acBtn.textContent = display_num === 0 ? "AC" : "⌫";
     }
     //operator연산자
     if (btn.classList.contains("operator")) {
@@ -37,13 +43,14 @@ buttons.forEach((btn) => {
         prev_num = calculator(prev_num, current_operator, display_num);
       }
       current_operator = click_text;
-
-      result.textContent = display_num + click_text;
+      
       if (click_text === "=") {
         acBtn.textContent = 'AC';
         input.textContent = prev_num;
-        // input.innerHTML = 0;
+        return;
       }
+      //계산된 값과 식만 반환해주는 코드
+      result.textContent = prev_num + current_operator;
       display_num = 0;
     }
 
@@ -77,20 +84,20 @@ function currentClear () {
   if (display_num !== '0') {
     //display_num을 ...으로 펼친 . 후 toStirg으로 문자열로 바꿔줌
     //.slice()함수로 0부터 -2번째까지 마지막 입력한 문자 하나를 삭제
-    //삭제 후 join으로 다시 문자열을 합쳐주고 Numger로 문자열을 다시 정수로 변환
+    //삭제 후 join으로 다시 문자열을 합쳐주고 Number()로 문자열을 다시 정수로 변환
     let currentArr = Number([...display_num.toString()].slice(0, -1).join(''));
     display_num = currentArr;
     input.textContent = currentArr;
 
-    //숫자를 다 삭제하면 Numger()함수를 통해 자동으로 display는 0으로 반환됨.
+    //숫자를 다 삭제하면 Number()함수를 통해 자동으로 display는 0으로 반환됨.
     if (display_num === 0) {
       acBtn.textContent = 'AC';
     }
-    console.log(display_num);
   }
 }
 
 function calculator(prev_num, current_operator, display_num) {
+  console.log(`c ${prev_num} ${current_operator} ${display_num}`);
   switch (current_operator) {
     case "+":
       return prev_num + display_num;
@@ -103,10 +110,6 @@ function calculator(prev_num, current_operator, display_num) {
     case "%":
       return prev_num % display_num;
     default:
-      return display_num;
+      return prev_num;
   }
 }
-
-
-//1.  삭제 함수 안에 전체삭제, 백스페이스 삭제 함수 선언
-//백스페이스에서 숫자가 하나 남았을 때 
